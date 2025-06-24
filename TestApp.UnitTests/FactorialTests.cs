0dad8e3953253cb571f2a58524cba5d950132385
Martin Kirov <fas7blas7@gmail.com>
2025-06-24 21:11:59 +0300

    Adding Factorial Tests

---
+++ TestApp.UnitTests/FactorialTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class FactorialTests
+{
+    [Test]
+    public void Test_CalculateFactorial_InputZero_ReturnsOne()
+    {
+        //Arrange
+        int inputNumber = 0;
+        int expected = 1;
+
+        //Act
+        int result = Factorial.CalculateFactorial(inputNumber);
+
+        //Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Test_CalculateFactorial_InputPositiveNumber_ReturnsCorrectFactorial()
+    {
+        //Arrange
+        int inputNumber = 5;
+        int expected = 120;
+
+        //Act
+        int result = Factorial.CalculateFactorial(inputNumber);
+
+        //Assert
+        Assert.AreEqual(expected, result);
+    }
+}
